7baf4c044ad7a5f92a69520c779da259c791b48c
thiagoAmorim2 <xavezamorim2@gmail.com>
2023-10-19 13:30:48 -0300

    implementação do método delete

--- src/main/java/com/desafios/controller/LimitesController.java
+++ src/main/java/com/desafios/controller/LimitesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,11 @@ public class LimitesController {
 			@RequestBody LimiteRequestDTO requestDTO
 			) {
 		return service.updateLimite(id, requestDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLimite(@PathVariable Long id) {
+		service.deleteLimite(id);
 	}
 
 }
